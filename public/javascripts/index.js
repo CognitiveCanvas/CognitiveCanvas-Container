f146bdb4bd13f43689982008ceb3031ff1f037a2
Unknown <xiw180@ucsd.edu>
2017-11-27 14:28:53 -0800

    Try to circumvent the cross-domain security protocol but failed
    Don't know why there are no message sent back. May be webstrates have something to do with this.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,23 @@
 function setSize() {
-    parent.document.getElementById('webstratesHere').style.height = document['body'].offsetHeight +'px';
-    parent.document.getElementById('webstratesHere').style.width = document['body'].offsetWidth +'px';
-
+    document.getElementById('webstratesHere').style.height = document['body'].offsetHeight +'px';
+    document.getElementById('webstratesHere').style.width = document['body'].offsetWidth +'px';
 }
+
+window.onmessage = function(event) {
+    console.log('receive message');
+    console.log(event.data);
+    document.getElementById("nodeList").innerHTML+=event.data;
+};
+
+function listElements() {
+    console.log(document.getElementById('webstratesHere').contentWindow);
+    document.getElementById('webstratesHere').contentWindow.postMessage('','*');
+}
+
+// for (i = 0; i < inside.nodes.length; i++){
+//     $('.nodeList').append( '<p id="Node-'+ i +'">' + inside.elements.nodes[i].data.id + '</p>' );
+// }
+//
+// for (i = 0; i < inside.edges.length; i++){
+//     $('.edgeList').append( '<p id="Edge-'+ i +'">' + inside.elements.nodes[i].data.id + '</p>' );
+// }
