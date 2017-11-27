bfa37e8f2f954a846ab9204d254d09d02d9ad4dc
Unknown <xiw180@ucsd.edu>
2017-11-27 10:59:07 -0800

    CM-25: View webstrates done
    Working on geting the node list

---
+++ public/javascripts/index.js
@@ -0,0 +1,5 @@
+function setSize() {
+    parent.document.getElementById('webstratesHere').style.height = document['body'].offsetHeight +'px';
+    parent.document.getElementById('webstratesHere').style.width = document['body'].offsetWidth +'px';
+
+}
