93d5ed8656b9adfacbcbe78912f46249903d4a18
Steve Zhao <xizhao@ucsd.edu>
2018-02-26 14:24:58 -0800

    set up backend structure for creating API

---
+++ server/services/ContainerAPI/config/app.js
@@ -0,0 +1,45 @@
+const express       = require('express'),
+      path          = require('path'),
+      morgan        = require('morgan'),
+      cookieParser  = require('cookie-parser'),
+      bodyParser    = require('body-parser'),
+      consign       = require('consign'),
+      app           = express(),
+      config        = require('./index.js'),
+      router        = express.Router(),
+      db            = require('./db.js')(config);
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cookieParser());
+
+//var keyword = require('../ContainerAPI/app/routes/keyword');
+
+//app.use('/searchKeyword', keyword);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+consign({ cwd: 'services' })
+      .include('ContainerAPI/app/setup')
+      //.then('ContainerAPI/app/routes')
+      .into(app);
+
+module.exports = app;
---
+++ server/services/ContainerAPI/config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret : 'containersecret',
+  session : { session : false },
+  database : 'mongodb://localhost:27017/CognitiveCanvas'
+}
