9461238ad8a8494d145266f558dc9541671dda57
Michael Tolentino <matolent@ucsd.edu>
2018-03-20 14:00:05 -0700

    Added bugsnag

--- client/src/app.vue
+++ client/src/app.vue
@@ -8,6 +8,9 @@
 export default {
   name: 'app'
 }
+  window.bugsnagClient = bugsnag('API_KEY')
+  // after Vue has been loaded…
+  bugsnagClient.use(bugsnag__vue(Vue))
 </script>
 
 <style lang="scss">
