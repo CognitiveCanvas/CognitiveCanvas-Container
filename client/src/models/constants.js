65f6e6a9fde7851ef75938f181c6402b07a8f0aa
Xavier (Xinwei Wang) <xinweixavier@gmail.com>
2018-05-23 12:56:44 -0700

    Template change to dev

--- client/src/models/constants.js
+++ client/src/models/constants.js
@@ -1,6 +1,7 @@
 const constants = {
   api: `http://${window.location.hostname}:8081/api`,
   template: `http://webstrates.ucsd.edu/rikudougorin-kurikaratenshou`,
+  //template: `http://webstrates.ucsd.edu/master`,
   host: 'http://webstrates.ucsd.edu/',
   baseWebstrateID: 'master'
 }
