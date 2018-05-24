4eaa89f58e72cea3052dd0b829ad0245e9e0109a
Michael Tolentino <michaelaaron.tolentino@gmail.com>
2018-05-24 13:19:22 -0700

    finished action logging, now includes user_id and saves to db

--- server/ContainerAPI/app/api/actionLog.js
+++ server/ContainerAPI/app/api/actionLog.js
@@ -4,17 +4,20 @@ const models = require('@ContainerManager/app/setup');
 const api = {}
 
 api.actionLog = (req, res) => {
-    let log = req.body;
-    // Create a new log
-    console.log('Action logged');
-    models.Action.create(log, function(err, newLog) {
-        if (err) {
-            console.log(err);
-            return handleError(err);
-            res.status(501).send(err);
-        }
-        res.send("success")
-    });
+    let log_list = req.body;
+    // Create a new action log
+    if (log_list.length > 0) {
+        // convert string to ObjectId
+        console.log('Action logged', log_list);
+        models.Action.create(log_list, function(err, newLog) {
+            if (err) {
+                console.log(err);
+                return handleError(err);
+                res.status(501).send(err);
+            }
+            res.send("Action Logged")
+        });
+    }
 }
 
 module.exports = api;
--- server/ContainerAPI/app/models/action.js
+++ server/ContainerAPI/app/models/action.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var actionSchema = new mongoose.Schema({
+    user_id: [{type:mongoose.Schema.Types.ObjectId, ref: 'User'}],
     map_id: String,
     interaction: String,
     event_type: Number,
