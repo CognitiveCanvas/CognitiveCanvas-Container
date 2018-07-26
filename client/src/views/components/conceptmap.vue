d91b59581a035c194e8ccd9e21d9d9c01313d45e
Xavier (Xinwei Wang) <xinweixavier@gmail.com>
2018-07-26 14:52:33 -0700

    Systemize Receiving & Processing Webstrate Message #45
    TODO: Post-process returned node/edges

--- client/src/views/components/conceptmap.vue
+++ client/src/views/components/conceptmap.vue
@@ -20,12 +20,13 @@
           // IMPORTANT: Check the origin of the data! 
           if (~event.origin.indexOf('https://webstrates.ucsd.edu')) { 
             // The data has been sent from your site 
-
+            
             if (event.data.id == "action_log") {
               // Save action-log to db
               event.data.data["user_id"] = self.$store.state.localUser.localUser.email._id;
               self.$data.temp_buffer.push(event.data.data);
-            } else if (event.data == "post_action_log") {
+            } 
+            else if (event.data == "post_action_log") {
               // Receive post request from webstrates, flush db
               let action_log = self.$data.temp_buffer;
               if (action_log.length > 0) {
@@ -33,18 +34,25 @@
                 self.$data.temp_buffer = [];
                 action_log = [];
               }
-            } else {
+            } 
+            else if (event.data.id == "selected_element") {
               // The data sent with postMessage is stored in event.data 
-              //console.log(event.data); 
               self.$store.dispatch("content/queryContent", {
-                elementUrl: event.data.id,
+                elementUrl: event.data.uid,
                 label: event.data.label
               });
               
               self.$store.dispatch("map/selectNode", {
-                nodeId: event.data.id,
+                nodeId: event.data.uid,
                 nodeLabel: event.data.label
               })
+            } 
+            else if (event.data.id == "related_element") {
+              // TODO: Process the related element
+              
+              
+              
+              
             }
             
           } 
