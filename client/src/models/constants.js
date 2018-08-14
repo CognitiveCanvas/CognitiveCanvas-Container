e14e8d55502cfbd1c82d25667d35ecc7d7b43282
Xavier (Xinwei Wang) <xinweixavier@gmail.com>
2018-08-14 10:30:20 -0700

    Recover Master to Dev Environment

--- client/src/models/constants.js
+++ client/src/models/constants.js
@@ -1,7 +1,7 @@
 const constants = {
   api: `http://${window.location.hostname}:8081/api`,
-//  template: `http://webstrates.ucsd.edu/master`,
-  template: `https://webstrates.ucsd.edu/rikudougorin-kurikaratenshou`, // Xavier_dev
+  template: `https://webstrates.ucsd.edu/master`,
+//  template: `https://webstrates.ucsd.edu/rikudougorin-kurikaratenshou`, // Xavier_dev
   noteTemplate: `https://webstrates.ucsd.edu/sarasvati-melt-out`,
   invalidNoteTemplate: `https://webstrates.ucsd.edu/shuten-doujin/`,
   host: 'https://webstrates.ucsd.edu/',
